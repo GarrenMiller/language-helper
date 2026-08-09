b85dce723898363a100d2081a5cd722f8eb20cd6
Garren Miller <garrenkmiller@gmail.com>
2026-08-08 23:33:45 -0700

    checkin: parsing works now

--- src/morphology/header.rs
+++ src/morphology/header.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, Read, Seek, BufReader, BufRead};
+use std::io::{self, Read, Seek, SeekFrom, BufReader, BufRead};
 use std::error::Error;
 use deku::prelude::*;
 
@@ -13,40 +13,17 @@ struct HfstMetadata {
 struct HfstPropertyHeader {
     num_input_symbols: u16,
     num_symbols: u16,
-    transition_index_length: u32,
-    transition_table_length: u32,
+    transition_index_size: u32,
+    transition_table_size: u32,
     num_states: u32,
-    
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    weighted: bool,
-   
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    deterministic: bool,
-   
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    input_deterministic: bool,
-
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    minimized_automaton: bool,
-    
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    cyclic: bool,
-    
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    double_epsilon_transition: bool,
-   
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    epsilon_x_transition: bool,
-    
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    epsilon_x_cycles: bool,
-   
-    #[deku(map = "|v: u32| -> Result<_, DekuError> { Ok(v != 0) }")]
-    unweighted_epsilon_x_cycles: bool
+    num_transitions: u32,
+    multichar_symbol_length: u32,
+    flags: u32,
+    property_mask: u32
 }
 
 pub fn read_header() -> io::Result<()> {
-    let file = File::open("test_sample.hfstol")?;
+    let file = File::open("hu.hfstol")?;
     let mut reader = BufReader::new(file);
     let mut magic = [0u8; 5];
     reader.read_exact(&mut magic)?;
@@ -65,16 +42,19 @@ fn is_modern_hfst(bytes: &[u8]) -> bool {
 }
 
 fn decode_hfst_metadata_header(mut reader: impl BufRead + Seek) -> Result<Vec<HfstMetadata>, Box<dyn Error>> {
-    let buffer = reader.fill_buf()?;
-    if buffer.is_empty() {
-        eprintln!("The file buffer was empty when decoding properties");
-    }
 
-    let length = u16::from_le_bytes([buffer[0], buffer[1]]);
-    let offset = length + 3; // Skip null byte separator
-    
-    let properties = &buffer[3..offset as usize]; 
-    let props_str = str::from_utf8(properties);
+    reader.seek(SeekFrom::Current(3))?; // We don't need length, just read until null byte pair
+   
+    let (index, props_str) = {
+        let buffer = reader.fill_buf()?;
+        if buffer.is_empty() {
+            eprintln!("The file buffer was empty when decoding properties");
+        }
+        let index = buffer.windows(2).position(|w| w == b"\0\0").unwrap();
+        let properties = &buffer[..index + 2];
+        (index, str::from_utf8(properties))
+    };
+
     let split = props_str?.split('\0').collect::<Vec<&str>>(); // Remove separators
 
     let result = split 
@@ -87,12 +67,12 @@ fn decode_hfst_metadata_header(mut reader: impl BufRead + Seek) -> Result<Vec<Hf
             _ => None,
         })
         .collect::<Vec<_>>();
-    reader.consume(offset as usize);
+    reader.consume(index + 2 as usize);
     return Ok(result);
 } 
 
 fn decode_hfst_property_header(mut reader: impl BufRead + Seek) -> Result<HfstPropertyHeader, Box<dyn Error>> {
     let buffer = reader.fill_buf()?;
-    let ((remaining_bytes, bit_offset), result) = HfstPropertyHeader::from_bytes((&buffer[..56], 0)).unwrap();
+    let ((remaining_bytes, bit_offset), result) = HfstPropertyHeader::from_bytes((&buffer[..32], 0)).unwrap();
     return Ok(result);
 }
