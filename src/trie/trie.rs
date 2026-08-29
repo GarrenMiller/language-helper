f487f4e5071a0f8df15d4388a8a2ac2516130ccd
Garren Miller <garrenkmiller@gmail.com>
2026-08-29 14:51:42 -0700

    checkin: more efficient insertion

--- src/trie/trie.rs
+++ src/trie/trie.rs
@@ -17,6 +17,7 @@
 //
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicU64, Ordering};
+use log::info;
 
 static NEXT_NODE_ID: AtomicU64 = AtomicU64::new(0);
 
@@ -59,49 +60,50 @@ impl Trie {
     }
 
     pub fn add_token(&mut self, token: &str) {
-        let mut bytes = token.bytes();
-        let first_byte = bytes.next().unwrap();
-        let first_node = Node::new(&first_byte);
+        let prefix = self.find_longest_prefix_path(token);
 
-        let mut parent_id = first_node.id;
-        self.children
-            .entry(self.root_id)
-            .or_insert_with(Vec::new)
-            .push(first_node.id);
-        self.nodes.insert(first_node.id, first_node);
+        info!("Prefix is: {:?}", prefix);
+
+        let (bytes, mut node_index) = match prefix {
+            Some(prefix) => (&token.as_bytes()[prefix.len()..], **prefix.last().expect("")),
+            None => (token.as_bytes(), self.root_id) 
+        };
 
         for byte in bytes {
             let next_node = Node::new(&byte);
-            parent_id = next_node.id;
             self.children
-                .entry(parent_id)
+                .entry(node_index)
                 .or_insert_with(Vec::new)
                 .push(next_node.id);
+            node_index = next_node.id;
             self.nodes.insert(next_node.id, next_node);
+            info!("Inserting node for {:?}", byte);
         }
     }
 
-    pub fn find_longest_prefix_path(&mut self, token: &str) {
+    pub fn find_longest_prefix_path(&mut self, token: &str) -> Option<Vec<&u64>> {
         let mut bytes = token.bytes();
         let first_byte = bytes.next().unwrap();
-        let first_node = self.children
-            .get(&self.root_id)
-            .expect("Do not try to access root node children before they exist")
-            .iter()
-            .find(|id| self.nodes.get(id).expect("All nodes must have an ID; something is wrong").value == first_byte);
+        let first_node = match self.children.get(&self.root_id) {
+            Some(children) => {
+                let root_children = children.iter().map(|id| self.nodes.get(id)).collect::<Vec<Option<&Node>>>();
+                info!("Root children are: {:?}", root_children);
+                info!("Matches: {:?}", root_children[0].unwrap().value == first_byte);
+                children.iter().find(|id| self.nodes.get(id).expect("All nodes must have an ID; something is wrong").value == first_byte)
+            },
+            None => return None,
+        };
 
-        if first_node.is_none() {
-            println!("TODO: Return empty string to indicate that no prefix is known");
-            return;
-        }
+        info!("Made it this far");
 
         let mut current_node = first_node.expect("current node has to exist");
         let mut path = vec![current_node];
         for byte in bytes {
             let current_children = self.children.get(current_node);
             match current_children {
-                None => println!("There are no children"),
+                None => return Some(path),
                 Some(children) => {
+                    info!("Made it to current children");
                     let next_node = children
                         .iter()
                         .find(|id| self.nodes.get(id).expect("All nodes must have an ID; something is wrong").value == byte);
@@ -109,14 +111,17 @@ impl Trie {
                     if let Some(value) = next_node {
                         path.push(value);
                         current_node = value;
-                        return;
+                        info!("checkpoint 1");
+                        continue;
                     }
 
-                    println!("There are children, but none have the correct value");
-                    return;
+                    info!("There are children, but none have the correct value");
+                    return None;
                 }
             }
         }
+        info!("Should return a list here");
+        Some(path)
     }
 }
 
@@ -130,8 +135,9 @@ mod tests {
     #[test]
     fn test_trie_add_token() {
         let mut trie = Trie::new();
-        trie.add_token("cat");
+        trie.add_token("car");
         info!("Nodes are: {:?}", trie.nodes);
+        trie.add_token("cart");
         info!("Children are: {:?}", trie.children);
         assert_eq!(true, true);
     }
